0f04414806d49e4c32b1ab815ac1b928ee0814c7
TurjoSiam <t.siam24@gmail.com>
2025-05-07 22:54:11 +0600

    [ADDED]-solution of problem  1 added

---
+++ solutions.ts
@@ -0,0 +1,10 @@
+
+function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper === undefined || toUpper === true) {
+        return input.toUpperCase();
+    }
+    else {
+        return input.toLowerCase();
+    };
+};
+
